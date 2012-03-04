a9edcb1ad8001db7072f0220bdb546cc05f31ac3
Baptiste Auguie <baptiste.auguie@gmail.com>
2012-03-04 00:00:00 +0000

    version 0.2.2

--- demo/Si.r
+++ demo/Si.r
@@ -1,14 +1,13 @@
 library(dielectric)
 library(ggplot2)
 
-data(AuJC)
+data(aSi)
 
+aSi$set_span(300, 800)
+raw <- dielectric2plot(aSi$raw())
+silicon <- aSi$predict(n=300, all.knots=TRUE)
 
-AuJC$set_span(300, 800)
-raw <- dielectric2plot(AuJC$raw())
-silver <- AuJC$predict(n=300, all.knots=TRUE)
-
-d <- dielectric2plot(silver)
+d <- dielectric2plot(silicon)
 
 ggplot(d, aes(wavelength, value)) + geom_path() +
   facet_grid(variable~., scales="free") +
